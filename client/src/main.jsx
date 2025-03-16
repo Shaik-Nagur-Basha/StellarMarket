c4401803352baa3493ab8e1cf3c3ac5a671a81eb
Shaik Nagur Basha <sknbasknba@gmail.com>
2025-03-16 22:13:38 +0530

    Remove React.StrictMode wrapper from main rendering in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,9 @@ import "./styles/utilities.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );

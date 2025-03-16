acfeec85839c41fff18e6509bb74bcada79e65d8
Shaik Nagur Basha <sknbasknba@gmail.com>
2025-03-16 19:50:15 +0530

    Trying to fix the error while deploy,, Update Express server static file paths and modify package scripts for improved deployment

--- api/index.js
+++ api/index.js
@@ -5,12 +5,12 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
-
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
 });
